e9dc129cb7eddfb556098023dee67b44bb60ba9e
Janhvi Singh <63140742+janhninini@users.noreply.github.com>
2020-07-10 00:21:33 +0530

    Add files via upload

---
+++ pages/choose_location.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:worldtime/services/world_time.dart';
+
+class ChooseLocation extends StatefulWidget {
+  @override
+  _ChooseLocationState createState() => _ChooseLocationState();
+}
+
+class _ChooseLocationState extends State<ChooseLocation> {
+
+List<WorldTime> locations = [
+  WorldTime(url: 'Europe/London', location: 'London', flag: 'uk.png'),
+  WorldTime(url: 'Europe/Athens', location: 'Athens', flag: 'greece.png'),
+  WorldTime(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.png'),
+  WorldTime(url: 'America/Vancouver', location: 'Vancouver', flag: 'canada.png'),
+  WorldTime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
+  WorldTime(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
+  WorldTime(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
+  WorldTime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
+  WorldTime(url: 'Asia/Shanghai', location: 'Shanghai', flag: 'china.png'),
+  WorldTime(url: 'Asia/Hong_Kong', location: 'Hong Kong', flag: 'hong_kong.png'),
+  WorldTime(url: 'America/Sao_Paulo', location: 'Sao Paulo', flag: 'brazil.png'),
+  WorldTime(url: 'America/Argentina/Buenos_Aires', location: 'Buenos Aires', flag: 'argentina.png'),
+  WorldTime(url: 'Asia/Tokyo', location: 'Tokyo', flag: 'japan.png'),
+  WorldTime(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
+  WorldTime(url: 'Asia/Taipei', location: 'Taipei', flag: 'taiwan.png'),
+  WorldTime(url: 'America/Toronto', location: 'Toronto', flag: 'canada.png'),
+  WorldTime(url: 'Asia/Kolkata', location: 'Kolkata', flag: 'india.png'),
+
+];
+
+void  updateTime(index) async {
+  WorldTime instance = locations[index];
+  await instance.getTime();
+  // navigate to home screen
+  Navigator.pop(context, {
+    'location': instance.location,
+    'flag': instance.time,
+    'time': instance.time,
+    'isDaytime': instance.isDaytime,
+  });
+}
+
+
+  @override
+  Widget build(BuildContext context) {
+    print("build function run");
+    return Scaffold(
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Choose a Location'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: ListView.builder(
+          itemCount:  locations.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 5.0),
+              child: Card(
+                child: ListTile(
+                  onTap: () {
+                    updateTime(index);
+                  },
+                  title: Text(locations[index].location),
+                  leading: CircleAvatar(
+                    backgroundImage: AssetImage('assets/${locations[index].flag}'),
+                  ),
+                ),
+              ),
+            );
+          }),
+    );
+  }
+}
